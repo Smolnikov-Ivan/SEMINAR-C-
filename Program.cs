058096e1a509a1b6762bb3872ed135d48a94fcc0
IVAN <smolnikov.ivan@lenta.ru>
2022-08-05 21:45:06 +0300

    сложная програма работает

--- Program.cs
+++ Program.cs
@@ -4,22 +4,31 @@
 //bool c = true; //false
 //float d = 15.4f;
 //double e = 15.4f;
+//string name = Console.RedLine();
+//Console.ReadLine() ждём значение из терминала
+//Convert.ToInt32() превращаем значение в число из терминала
+//Создаём переменную и записываем значение туда 
+// = присвоить
+// == сравнить
+// if() {}
 
 while (true)
 {
+    Console.WriteLine("Введите оператор +/-/*/: или end для выхода");
+    string Operator = Console.ReadLine();
+    if (Operator == "end");
+    {
+        break;
+    }
     Console.WriteLine("Введите 1 число");
-    //string name = Console.RedLine();
+   
     int number1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите 2 число");
-    //Console.ReadLine() ждём значение из терминала
-    //Convert.ToInt32() превращаем значение в число из терминала
-    //Создаём переменную и записываем значение туда 
+
     int number2 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите оператор +/-/*/:");
-    string Operator = Console.ReadLine();
-    // = присвоить
-    // == сравнить
-    // if() {}
+ 
+   
+
     if (Operator == "+")
     {
         Console.WriteLine("Числа сложены, Иван Алексеевич!");
@@ -40,7 +49,9 @@ while (true)
         Console.WriteLine("Числа разделены, Иван Алексеевич!");
         Console.WriteLine(number1 / number2);
     }
+    break;
 }
+
 
 
 
